5fc03174a39e06948a354128ce64ae8af355fef8
Chanakyasinde <chanakyasinde@gmail.com>
2026-03-03 22:42:30 +0530

    intial commit

---
+++ dist/cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const { Command } = require('commander');
+const axios = require("axios");
+const program = new Command();
+program
+    .command("greet <name>")
+    .action((name) => console.log(`Hello ${name}`));
+program
+    .command("add <n1> <n2>")
+    .description("Add two numbers")
+    .action((n1, n2) => console.log(Number(n1) + Number(n2)));
+program
+    .command("joke")
+    .description("Random joke")
+    .action(async () => {
+    try {
+        const res = await axios.get(`https://official-joke-api.appspot.com/random_joke`);
+        console.log(res.setup);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+program.parse();
